a139bf26c6ac11b00dbfad2849a3cc5fa64e0a43
Code-With-Amine <userihj36@gmail.com>
2023-04-02 18:18:50 +0000

    adding the score future which allow the user to see his/her score in the game

--- js/script.js
+++ js/script.js
@@ -39,78 +39,107 @@ const keys = [
 
 // End of the keyboard
 
+let score = 0
 // counting the user mistakes
-var mistakes = 0;
+let mistakes
 
 // function that return a random item from the array
 function getRandomItem(){
     let Randomindex = Math.floor(Math.random() * fruits.length);
     return fruits[Randomindex];
 }
+let fruits
+let item
+let placeholders
+let itemArr
+let collectionOfLetters
 
-let fruits = ["kiwi", "apple", "mango","carrot","brocoli","onion","ketchup", "banana", "apricot", "cherry", "figs", "jujube", "orange", "papaya", "plum"];
-//getting a random item
-var item = document.getElementById("guessed").value = getRandomItem();
-const itemArr = Array.from(item)
+function setGame(){
+    mistakes = 0
 
-//create a placeholder for the correct words
 
-var placeholders = document.getElementById("correctLetters");
-for(i in item){
-    const LetterPlaceholder = document.createElement("div");
-    LetterPlaceholder.setAttribute("class","col letter")
-    placeholders.appendChild(LetterPlaceholder);
+    fruits = ["kiwi", "apple", "mango","carrot","brocoli","onion","ketchup", "banana", "apricot", "cherry", "figs", "jujube", "orange", "papaya", "plum"];
+    //getting a random item
+    item = document.getElementById("guessed").value = getRandomItem();
+    itemArr = Array.from(item)
+
+    //create a placeholder for the correct words
+
+    placeholders = document.getElementById("correctLetters");
+
+    for(i in item){
+        const LetterPlaceholder = document.createElement("div");
+        LetterPlaceholder.setAttribute("class","col letter")
+        placeholders.appendChild(LetterPlaceholder);
+    }
+    collectionOfLetters = document.getElementsByClassName('letter')
+
 }
-var collectionOfLetters = document.getElementsByClassName('letter')
+
+
+setGame()
+
+// set the user score
+
+function setScore(score){
+    document.getElementById('score').innerText = score;
+}
+
+
+setScore(score);
+
+let userGuess = '';
 
 
 // checking the user input
 function checkGuess(){
-    var userGuess = (document.getElementById("userguss").value).toLowerCase();
+            userGuess = (document.getElementById("userguss").value).toLowerCase();
 
-    var IsthereLetter = false;
-    for(i in userGuess){
-        for(j in itemArr){
-            // I'm checking if the item includes that letter
-            if(userGuess[i] === itemArr[j]){
-                collectionOfLetters[j].innerText = userGuess[i];
-                IsthereLetter = true;
+            var IsthereLetter = false;
+            for(i in userGuess){
+                for(j in itemArr){
+                    // I'm checking if the item includes that letter
+                    if(userGuess[i] === itemArr[j]){
+                        collectionOfLetters[j].innerText = userGuess[i];
+                        IsthereLetter = true;
+                    }
+                }
+                }
+        
+                clearUserGuess();
+
+                if(IsthereLetter === false){
+                    mistakes ++;
+                    (document.getElementById('Hangman')).style.display = " block";
+                    try{
+                    (document.getElementById(`mistake-${mistakes}`)).style.display = " block";
+                    }
+                    catch(err){
+                            // Do nothing (catch the error and move on)
+
+                    }
+                }
+                var Guess = '';
+                for( i in collectionOfLetters){
+                    if(collectionOfLetters[i].innerText !== undefined){
+                        Guess += collectionOfLetters[i].innerText;
+                    }
+                }
+
+                if(Guess.length == item.length && Guess === item){
+
+                        correctAnswer()
+                    }
+
+                if(mistakes >= 5){
+                    endGame();
+                    score --;
+                    setScore(score);
+                }
+
+                disableLetters(userGuess)
             }
-        }
-        }
-   
-        document.getElementById("userguss").value = " ";
 
-        if(IsthereLetter === false){
-            mistakes ++;
-            (document.getElementById('Hangman')).style.display = " block";
-            (document.getElementById(`mistake-${mistakes}`)).style.display = " block";
-        }
-        var Guess = '';
-        for( i in collectionOfLetters){
-            if(collectionOfLetters[i].innerText !== undefined){
-                Guess += collectionOfLetters[i].innerText;
-            }
-        }
-
-        if(Guess.length == item.length){
-            if(Guess === item){
-                correctAnswer()
-            }
-            else{
-                document.getElementById("guessed").type = "text"
-                showLoserIcon()
-            }
-    }
-
-        if(mistakes >= 5){
-            document.getElementById('userguss').readOnly = true;
-            document.getElementById("check").disabled = true;
-            document.getElementById("guessed").type = "text";
-        }
-
-        disableLetters(userGuess)
-    }
 
 function disableLetters(disabledLetters){
     let disabledLettersArry =  Array.from(disabledLetters)
@@ -123,8 +152,11 @@ function disableLetters(disabledLetters){
 
 function correctAnswer(){
     (document.getElementById('Hangman')).style.display = " none";
-    document.getElementById("guessed").type = "text"
+    changeType('text')
+    score ++;
+    setScore(score);
     showWinIcon();
+    endGame();
 }
 
 
@@ -134,6 +166,71 @@ function showWinIcon(){
 }
 
 
+function changeType(word){
+    document.getElementById("guessed").type = word;
+}
+
+
+function clearUserGuess(){
+
+    document.getElementById("userguss").value = " ";
+
+}
+
+
+function endGame(read_Only = true, disable = true, word = 'text' ){
+            document.getElementById('userguss').readOnly = read_Only;
+            document.getElementById("check").disabled = disable;
+            changeType(word);
+}
+
+
+function enableLetters(){
+
+    for(i of keys){
+        i.forEach(letter =>{ 
+            document.getElementById(letter).disabled = false;
+        })
+    }
+}
+
+function clearInputs(){
+    document.getElementById('guessed').innerText = '';
+    document.getElementById('userguss').innerText = '';
+    changeType('password')
+    for(i of collectionOfLetters){
+        i.innerHTML = ''
+    }
+
+}
+
+
+function clearPicturs(){
+    document.getElementById('Hangman').style.display = " none";
+    for(let i = 1 ; i <= 5 ; i++){
+        (document.getElementById(`mistake-${i}`)).style.display = " none";
+    }
+    document.getElementById("winner").style.display = " none";
+
+}
+
+
+function removeOladPlaceholders() {
+    while (placeholders.firstChild) { // remove all the children of the placeholders one by one untill no children left
+        placeholders.removeChild(placeholders.firstChild);
+    }
+}
+
+
 function PlayAgain(){
-    location.reload();
+    clearPicturs()
+    enableLetters()
+    removeOladPlaceholders()
+    changeType('password')
+    clearInputs()
+    endGame(false, false,'password')
+    setGame()
+    mistakes = -1
+    checkGuess()
+
 }
